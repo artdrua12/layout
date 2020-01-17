c0400638527d8f7268fd6d6ab62dfcc22f3cd54a
artdrua12 <artdrua@rambler.ru>
2020-01-17 19:40:54 +0300

    third commit

--- layout/src/main.js
+++ layout/src/main.js
@@ -1,5 +1,14 @@
 import Vue from 'vue'
+// element
+import ElementUI from 'element-ui';
+import locale from 'element-ui/lib/locale/lang/en'
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.use(ElementUI, { locale })
+
 import App from './App.vue'
+
+
 
 new Vue({
   el: '#app',
